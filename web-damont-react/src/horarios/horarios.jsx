04efb68cc4eca26dd34a8c402caecca31b5695ed
dario-montenegro <dariiocjs@gmail.com>
2026-06-17 12:38:10 -0300

    creacion de horarios

---
+++ web-damont-react/src/horarios/horarios.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function horarios() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default horarios
---
+++ web-damont-react/src/pages/HorariosAtancion.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HorariosAtencion() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HorariosAtencion
